89e524161d6cf737f4c772a702ddcd5493d32a60
nnbinh <nnbinh0301@gmail.com>
2021-07-19 00:48:30 +0700

    implement "search" function and update ui

--- cryptowallet/app/src/main/java/hcm/nnbinh/cryptowallet/repo/PriceRepo.kt
+++ cryptowallet/app/src/main/java/hcm/nnbinh/cryptowallet/repo/PriceRepo.kt
@@ -6,7 +6,7 @@ import hcm.nnbinh.cryptowallet.database.entity.Price
 
 class PriceRepo(private val apiService: ApiService, private val db: CryptoDatabase) {
 	suspend fun getRemotePriceList() = apiService.getAllPrices()
-	suspend fun insert(items: List<Price>) = db.getPriceDao().insert(*items.toTypedArray())
-	fun getAllLive() = db.getPriceDao().getAllLive()
+	fun insert(items: List<Price>) = db.getPriceDao().insert(*items.toTypedArray())
+	fun getPriceListFlow(word: String) = db.getPriceDao().getPricesFlow("%$word%")
 	fun getPrice(base: String, name: String) = db.getPriceDao().getPriceLive(base, name)
 }
--- cryptowallet/app/src/main/java/hcm/nnbinh/cryptowallet/screens/price_list/PriceListFragment.kt
+++ cryptowallet/app/src/main/java/hcm/nnbinh/cryptowallet/screens/price_list/PriceListFragment.kt
@@ -2,18 +2,32 @@ package hcm.nnbinh.cryptowallet.screens.price_list
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.lifecycle.lifecycleScope
+import hcm.nnbinh.cryptowallet.R
+import hcm.nnbinh.cryptowallet.base.BaseActivity
 import hcm.nnbinh.cryptowallet.base.BaseFragment
 import hcm.nnbinh.cryptowallet.databinding.FragmentPriceListBinding
 import hcm.nnbinh.cryptowallet.helper.networkAvailableFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class PriceListFragment : BaseFragment() {
+
+@ExperimentalCoroutinesApi
+class PriceListFragment : BaseFragment(), OnQueryTextListener {
 	private lateinit var binding: FragmentPriceListBinding
 	private val priceListVM: PriceListVM by viewModel()
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setHasOptionsMenu(true)
+	}
 	
 	override fun onCreateView(
 		inflater: LayoutInflater,
@@ -32,7 +46,18 @@ class PriceListFragment : BaseFragment() {
 		return binding.root
 	}
 	
+	override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+		inflater.inflate(R.menu.mn_price_list, menu)
+		setupSearchView(menu)
+	}
+	
+	private fun setupSearchView(menu: Menu) {
+		val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+		searchView.setOnQueryTextListener(this)
+	}
+	
 	override fun setupViews() {
+		(requireActivity() as BaseActivity).setupActionBar(binding.toolbar, false)
 		binding.rcvPrices.adapter = PriceListAdapter()
 	}
 	
@@ -40,8 +65,18 @@ class PriceListFragment : BaseFragment() {
 		priceListVM.getCommand().observe(this) { cmd ->
 			processCommand(cmd)
 		}
-		priceListVM.priceList.observe(this) { newList ->
+		priceListVM.displayPriceList.observe(this) { newList ->
 			(binding.rcvPrices.adapter as PriceListAdapter).submitList(newList)
 		}
+	}
+	
+	override fun onQueryTextSubmit(query: String?): Boolean {
+		priceListVM.filterPricesBy(query)
+		return true
+	}
+	
+	override fun onQueryTextChange(newText: String?): Boolean {
+		priceListVM.filterPricesBy(newText)
+		return true
 	}
 }
